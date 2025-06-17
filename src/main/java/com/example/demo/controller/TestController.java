794d00423363a277fe7b16a9405b28d13ad22590
MartinOM321753 <20223tn139@utez.edu.mx>
2025-06-17 16:23:58 -0600

    [FIRST-COMMIT]|Subida Inicial del proyecto

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/tests")
+public class TestController {
+
+    @GetMapping("")
+    public String getTestsMessage() {
+        return "API WORKING..";
+    }
+
+}
